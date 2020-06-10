8f24c12d07aef444bbd368d78bc9179643176276
Alex Foreman <foreman.alexander@gmail.com>
2020-06-10 17:48:35 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot("Squawk")
+  puts "Squawk!"
+end
+
+def parrot(phrase= "Squawk!")
+  puts #{phrase}
+end
+parrot("Pretty Bird!")
